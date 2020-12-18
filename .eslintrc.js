0fec86c2f70eb4f658877a953bed0831aae323f3
Su Anli <sual0001@gmail.com>
2020-12-18 13:53:01 +0800

    chore(screens, package): add react navigation, home screen

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,8 @@ module.exports = {
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  env: {
+    'react-native/react-native': true,
+    'jest/globals': true,
+  },
 };
